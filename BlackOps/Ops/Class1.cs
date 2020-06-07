3b7ecbfe2d1ae2649dc2600d0c35129be7e84d79
Héctor López <hector.lopez@DSA-DEV2-10>
2020-06-07 00:03:42 -0500

    load firebase conn

--- BlackOps/Ops/Class1.cs
+++ BlackOps/Ops/Class1.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ops
+﻿namespace Ops
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FireSharp.Config;
+    using FireSharp.Interfaces;
+    using FireSharp.Response;
+
     public class Class1
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "ialdMTdBT4y5CRpzTAO2AuoDhZF4R5A4YMhbrCig",
+            BasePath = "https://blackops-63cb7.firebaseio.com/"
+        };
+
+        IFirebaseClient firebaseClient;
     }
 }
